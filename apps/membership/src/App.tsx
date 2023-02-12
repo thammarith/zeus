295a431932b987f1c67d7c78c31a1044cddda417
Thammarith <8011543+thammarith@users.noreply.github.com>
2023-02-12 16:47:29 +0700

    Add membership

--- apps/membership/src/App.tsx
+++ apps/membership/src/App.tsx
@@ -7,11 +7,13 @@ function App() {
 
   return (
     <div className="App">
+          <h1 className="text-3xl font-bold underline">sdfsfsfsdf</h1>
+      
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
-        <a href="https://reactjs.org" target="_blank">
+        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
