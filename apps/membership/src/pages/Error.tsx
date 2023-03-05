10b2487336ab03a211bdc1bdcc7675da8408edd6
Thammarith <8011543+thammarith@users.noreply.github.com>
2023-03-05 16:15:56 +0700

    Publish

--- apps/membership/src/pages/Error.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Error = () => {
-    return <div>Houston, we&apos;ve had a problem!</div>;
-};
-
-export default Error;
--- apps/membership/src/routes/index.ts
+++
@@ -1,5 +0,0 @@
-export const INDEX_PATH = '/';
-export const ERROR_PATH = '/error';
-export const AUTHENTICATE_PATH = '/authenticate';
-export const PROFILE_PATH = '/profile';
-export const PROFILE_EDIT_PATH = '/profile/edit';
